4ea8b4f64ea72a110027eb6cdf36aac630f601ea
alluballu <axu.porri@gmail.com>
2022-03-04 23:28:18 +0200

    Started working on the progress bar component

---
+++ pages/Components/ProgressBar.jsx
@@ -0,0 +1,8 @@
+const ProgressBar = () => {
+  return (
+    <div>
+      <div>test</div>
+    </div>
+  );
+};
+export default ProgressBar;
--- pages/screens/MyProgress.jsx
+++ pages/screens/MyProgress.jsx
@@ -1,14 +1,17 @@
 import { Box } from "@mui/material";
 import styles from "../../styles/Home.module.css";
 import Link from "next/link";
+import ProgressBar from "../Components/ProgressBar";
 
 const MyProgress = () => {
   const secondTempData = [{
+    type:"goal",
     goal: 100,
     current: 50,
     title: "Learn 100 new Spanish words"
     
-  },{
+  }, {
+    type:"no-goal",
     goal: 100,
     current: 52,
     title: "Learn 100 new Finnish words"
@@ -28,7 +31,7 @@ const MyProgress = () => {
           id={styles.goalBoxContainer}
         >
           {secondTempData.map((element, index) => {
-            return <div id={styles.goalBox} key={index}><div>{element.title}</div> {element.current}/{element.goal} </div>;
+            return <div id={styles.goalBox} key={index}><div>{element.title}</div> <ProgressBar/> {element.current}/{element.goal} </div>;
           })}
         </div>
       </Box>
